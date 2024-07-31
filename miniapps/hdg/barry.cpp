7ec62b04e07da4d88e2ca707674327814c6d758c
Barry Fadness <bfadness@pdx.edu>
2024-07-31 04:40:36 -0700

    Check matrix A22 against hdg_poisson

--- miniapps/hdg/barry.cpp
+++ miniapps/hdg/barry.cpp
@@ -48,6 +48,8 @@ int main(int argc, char* argv[])
     MixedBilinearForm b(&pressure_space, &velocity_space);
     b.AddDomainIntegrator(new VectorDivergenceIntegrator());
 
+    Table element_to_edge_table(mesh.ElementToEdgeTable());
+
     for (int element_index = 0; element_index < mesh.GetNE(); ++element_index)
     {
         cout << "Element_index: " << element_index << endl;
@@ -64,6 +66,55 @@ int main(int argc, char* argv[])
         A21.PrintMatlab();
         cout << endl;
 
+        const FiniteElement* velocity_element = velocity_space.GetFE(element_index);
+        const FiniteElement* pressure_element = pressure_space.GetFE(element_index);
+        DenseMatrix A22(pressure_element->GetDof());
+        A22 = 0.0;
+
+        Array<int> edge_indices_array;
+        element_to_edge_table.GetRow(element_index, edge_indices_array);
+        for (int i = 0; i < edge_indices_array.Size(); ++i)
+        {
+            const int edge_index(edge_indices_array[i]);
+            FaceElementTransformations* trans(
+                mesh.GetFaceElementTransformations(edge_index));
+            bool use_element_two(false);
+            if (trans->Elem2No == element_index)
+                use_element_two = true;
+
+            const FiniteElement* edge = auxiliary_space.GetFaceElement(edge_index);
+
+            const int quad_order = 5;
+            const IntegrationRule ir(IntRules.Get(trans->FaceGeom, quad_order));
+
+
+            for (int point_index = 0; point_index < ir.GetNPoints(); ++point_index)
+            {
+                const IntegrationPoint ip(ir.IntPoint(point_index));
+                trans->SetIntPoint(&ip);
+                // Vector edge_shape(edge->GetDof());
+                // edge->CalcShape(ip, edge_shape);
+
+                IntegrationPoint eip;
+                if (use_element_two)
+                    trans->Loc2.Transform(ip, eip);
+                else
+                    trans->Loc1.Transform(ip, eip);
+
+                // Vector velocity_element_shape(velocity_element->GetDof());
+                // velocity_element->CalcShape(eip, velocity_element_shape);
+
+                Vector pressure_element_shape(pressure_element->GetDof());
+                pressure_element->CalcShape(eip, pressure_element_shape);
+
+                const real_t weight = ip.weight*trans->Weight()*5.0;
+                AddMult_a_VVt(weight, pressure_element_shape, A22);
+
+                Vector nor(dim);
+                CalcOrtho(trans->Jacobian(), nor);
+            }
+        }
+        A22.PrintMatlab();
         cout << endl;
     }
     return 0;
